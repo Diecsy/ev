8e1c2be3f71a400b3f36820cfd41f989888dc988
lmao <116317098+Diecsy@users.noreply.github.com>
2025-05-11 22:42:33 -0700

    Add files via upload

---
+++ StartServer.bat
@@ -0,0 +1,10 @@
+
+java -Xmx2048M -Xms2048M -jar Alpha_1.1.2_01.jar nogui
+rem if exist "C:\Program Files (x86)\Java\jre6\bin"\java.exe goto THEEND
+
+
+rem if exist "C:\Program Files\Java\jre6\bin"\java.exe "C:\Program Files\Java\jre6\bin\"java -Xmx1024M -Xms1024M -jar CraftBukkit1060.jar nohud
+pause
+
+:THEEND
+exit
